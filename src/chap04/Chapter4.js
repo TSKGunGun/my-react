7b1967a5ac111b403d0d9c5b277fb36bc95e560c
TskGunGun <stonebeach.dakar@gmail.com>
2024-04-23 14:13:58 +0900

    4-1-4

--- src/chap04/Chapter4.js
+++ src/chap04/Chapter4.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import StateForm from './StateForm';
+import StateFormUC from './StateFormUC';
 
 export default function Chapter4Component() {
   return (
@@ -8,6 +9,10 @@ export default function Chapter4Component() {
       <div className='chapter'>
         <h2>4-1-2</h2>
         <StateForm />
+      </div>
+      <div className='chapter'>
+        <h2>4-1-4</h2>
+        <StateFormUC />
       </div>
     </>
   )
